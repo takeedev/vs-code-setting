30ee627395e46ae5820de09b23671eabf6468af8
taknaja <peerapolkoeram@gmail.com>
2025-01-30 23:42:30 +0700

    update echo

--- install-extensions.sh
+++ install-extensions.sh
@@ -9,8 +9,8 @@ code --force --install-extension eamodio.gitlens
 #vscode-eslint
 code --force --install-extension dbaeumer.vscode-eslint
 
+#echo 
+echo "++++++++++++++++++++++++++++++++++++++++"
+
 #check extension
 code --list-extensions
-
-#echo 
-echo "install extention complese"
