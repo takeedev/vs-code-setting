73f983cf8470bde96f01aa71f6a82b15efebc610
taknaja <peerapolkoeram@gmail.com>
2025-01-31 00:07:33 +0700

    add extension

--- install-extensions.sh
+++ install-extensions.sh
@@ -9,6 +9,15 @@ code --force --install-extension eamodio.gitlens
 #vscode-eslint
 code --force --install-extension dbaeumer.vscode-eslint
 
+#icon-theme
+code --force --install-extension PKief.material-icon-theme
+
+#vim
+#code --force --install-extension vscodevim.vim
+
+#mermadAid
+#code --force --install-extension ms-vscode.copilot-mermaid-diagram
+
 #echo 
 echo "++++++++++++++++++++++++++++++++++++++++"
 
