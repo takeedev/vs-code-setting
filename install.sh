25b9e69ec96f42963d42a22c2c9d652260a11410
taknaja <peerapolkoeram@gmail.com>
2025-01-30 22:41:06 +0700

    copy setting_test.xml to system

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 config_path_last_version=$(ls -d $HOME/AppData/Roaming/Code* | sort -V | tail -n 1)
 echo "Latest version: $config_path_last_version"
+
+# config
+curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/settings.json -o "$config_path_last_version/settings_test.xml"
