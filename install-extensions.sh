26424ebb31f136c9f69307ab3f14ac350d55a957
taknaja <peerapolkoeram@gmail.com>
2025-01-30 23:19:55 +0700

    add extension vscode-eslint

--- install-extensions.sh
+++ install-extensions.sh
@@ -2,4 +2,9 @@
 
 #gitlens
 code --force --install-extension eamodio.gitlens
+
+#vscode-eslint
+code --force --install-extension dbaeumer.vscode-eslint
+
+#echo 
 echo "install extention complese"
--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ echo "Latest version: $config_path_last_version"
 # config
 curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/settings.json -o "$config_path_last_version/User/settings.json"
 curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/keybindings.json -o "$config_path_last_version/User/keybindings.json"
+
+# execute install extendtion
 bash <(curl -sL https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/install-extensions.sh)
