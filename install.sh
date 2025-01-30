4fff2ba93da67494e7b6e765e38d33e47618ffb5
taknaja <peerapolkoeram@gmail.com>
2025-01-30 22:48:22 +0700

    update path

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ config_path_last_version=$(ls -d $HOME/AppData/Roaming/Code* | sort -V | tail -n
 echo "Latest version: $config_path_last_version"
 
 # config
-curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/settings.json -o "$config_path_last_version/code/settings_test.xml"
-curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/keybindings.json -o "$config_path_last_version/code/keybindings_test.xml"
+curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/settings.json -o "$config_path_last_version/User/settings_test.xml"
+curl -k https://raw.githubusercontent.com/takeedev/vs-code-setting/refs/heads/main/config/keybindings.json -o "$config_path_last_version/User/keybindings_test.xml"
